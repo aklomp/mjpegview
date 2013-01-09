71941179334355ca63d49465849a1c01eabbdcbe
Alfred Klomp <git@alfredklomp.com>
2013-01-09 23:24:27 +0100

    mjv_filename: const correctness for source name string
    mjv_source_get_name() returns a const char *const; make sure this
    function accepts it.

--- mjv_filename.h
+++ mjv_filename.h
@@ -1,6 +1,6 @@
 #ifndef MJV_FILENAME_H
 #define MJV_FILENAME_H
 
-char *mjv_filename_forge (char *const srcname, unsigned int framenum, char *const pat);
+char *mjv_filename_forge (const char *const srcname, unsigned int framenum, char *const pat);
 
 #endif	/* MJV_FILENAME_H */

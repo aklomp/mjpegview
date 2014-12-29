f0debb7c6139330bc14681c2900b04ac816e0a91
Alfred Klomp <git@alfredklomp.com>
2014-12-29 23:44:45 +0100

    Spinner: add pthread main routine

--- spinner.c
+++ spinner.c
@@ -1,6 +1,15 @@
+// Unlock clock_nanosleep() and friends:
+#define _POSIX_C_SOURCE 200112L
+
 #include <stdlib.h>
+#include <string.h>
 #include <pthread.h>
+#include <time.h>
 #include <cairo.h>
+
+#define STEPS 12
+#define ITERS_PER_SEC  (STEPS)
+#define INTERVAL_NSEC  (1000000000 / ITERS_PER_SEC)
 
 struct spinner
 {
@@ -11,6 +20,46 @@ struct spinner
 	struct timespec start;
 	pthread_t pthread;
 };
+
+static void *
+thread_main (void *userdata)
+{
+	struct spinner *s = userdata;
+	struct timespec now, wake;
+
+	// This function runs the spinner thread.
+	// Every interval, update spinner step and issue a tick callback.
+	// Continue till canceled.
+	for (;;)
+	{
+		// Get absolute time, calculated from the start time and the number
+		// of iterations, of when the next tick should be issued. Aligning
+		// the timing to an absolute clock prevents framerate drift.
+		wake.tv_sec  = s->start.tv_sec + s->iters / ITERS_PER_SEC;
+		wake.tv_nsec = s->start.tv_nsec + (s->iters % ITERS_PER_SEC) * INTERVAL_NSEC;
+		if (wake.tv_nsec >= 1000000000) {
+			wake.tv_sec++;
+			wake.tv_nsec -= 1000000000;
+		}
+		// `wake' holds the time that we should wake up at. If this time is
+		// already in the past, we are out of sync and rebase time to `now':
+		clock_gettime(CLOCK_REALTIME, &now);
+		if (now.tv_sec > wake.tv_sec || (now.tv_sec == wake.tv_sec && now.tv_nsec >= wake.tv_nsec)) {
+			s->iters = 0;
+			memcpy(&s->start, &now, sizeof(struct timespec));
+		}
+		else {
+			while (clock_nanosleep(CLOCK_REALTIME, TIMER_ABSTIME, &wake, NULL) != 0);
+		}
+		// Issue a tick callback:
+		s->on_tick(s->userdata);
+
+		// Continue:
+		s->step = (s->step + 1) % STEPS;
+		s->iters++;
+	}
+	return NULL;
+}
 
 struct spinner *
 spinner_create (void (*on_tick)(void *), void *userdata)
@@ -27,6 +76,11 @@ spinner_create (void (*on_tick)(void *), void *userdata)
 	s->on_tick = on_tick;
 	s->userdata = userdata;
 
+	// Start the spinner pthread:
+	if (pthread_create(&s->pthread, NULL, thread_main, s) != 0) {
+		free(s);
+		return NULL;
+	}
 	return s;
 }
 
@@ -36,6 +90,8 @@ spinner_destroy (struct spinner **s)
 	if (s == NULL || *s == NULL) {
 		return;
 	}
+	// Destroy spinner pthread:
+	pthread_cancel((*s)->pthread);
 	free(*s);
 	*s = NULL;
 }
